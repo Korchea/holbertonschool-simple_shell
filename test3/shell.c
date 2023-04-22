0350d70d0f9abb0abc9e89b6067be00bdd546868
Korchea <6334@holbertonstudents.com>
2023-04-22 15:02:50 -0700

    Shell.c little updates

--- test3/shell.c
+++ test3/shell.c
@@ -6,14 +6,21 @@
 
 int main(int argc, char **argv, char **envp)
 {
-    char *cmd = NULL, *taux = " \0", *tok[128] = {};
+    char *cmd = NULL, *taux = " \0", *token = NULL, *tok[128] = {};
     size_t size = 0;
+    int i = 0;
 
     while (printf("$ ") && getline(&cmd, &size, stdin) != -1)
     {
-        tok[0] = strtok(cmd, taux);
-        while (tok)
-        printf("%s", cmd);
+        token = strtok(cmd, taux);
+        while (token)
+        {
+            tok[i] = token;
+            printf("%s\n", tok[i]);
+            token = strtok(NULL, taux);
+            i++;
+        }
     }
+    free(cmd);
     return (0);
 }
