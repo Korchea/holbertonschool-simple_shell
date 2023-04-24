d19ed51ea854b367490daf55fae50c6e743b26b6
Nacho228 <6319@holbertonstudents.com>
2023-04-24 03:08:41 -0700

    a single detail that does not changes nothing functioanlly

--- test3/shell.c
+++ test3/shell.c
@@ -1,10 +1,4 @@
-#include <stdio.h>
-#include <sys/types.h>
-#include <sys/wait.h>
-#include <unistd.h>
-#include <stdlib.h>
-#include <string.h>
-#include <stdint.h>
+#include "shell.h"
 extern char **environ;
 
 /**
